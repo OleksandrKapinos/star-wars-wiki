19fb3144d967390b0341e93532ef2e2a128dc734
OleksandrKapinos <oleksandr.kapinos@gmail.com>
2020-09-08 12:27:38 +0300

    Updates

---
+++ precache-manifest.d26ba8133210bc9002870999401492f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbc5770af45f7c1500fe0f99695e29cd",
+    "url": "/https,://oleksandrkapinos.github.io/star-wars-wiki/index.html"
+  },
+  {
+    "revision": "1f058f4bd0c1636df901",
+    "url": "/https,://oleksandrkapinos.github.io/star-wars-wiki/static/css/main.4daa4c80.chunk.css"
+  },
+  {
+    "revision": "691483c37a915789bfd6",
+    "url": "/https,://oleksandrkapinos.github.io/star-wars-wiki/static/js/2.161af960.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/https,://oleksandrkapinos.github.io/star-wars-wiki/static/js/2.161af960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f058f4bd0c1636df901",
+    "url": "/https,://oleksandrkapinos.github.io/star-wars-wiki/static/js/main.017303a8.chunk.js"
+  },
+  {
+    "revision": "d1cd8e2e0d4a6fee21af",
+    "url": "/https,://oleksandrkapinos.github.io/star-wars-wiki/static/js/runtime-main.49870546.js"
+  }
+]);
